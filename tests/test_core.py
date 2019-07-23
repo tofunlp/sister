db11f82abc219a244216d4605787ae574f31a168
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-07-23 16:49:13 +0900

    Added test case for SentenceEmbedding

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,18 +1,43 @@
 from unittest import TestCase
 
+from sister.tokenizers import Tokenizer
+from sister.word_embedders import WordEmbedding
 from sister import MeanEmbedding
+from sister.core import SentenceEmbedding
+
+
+class SentenceEmbeddingCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_embed_not_implemented(self):
+        class Dummy(SentenceEmbedding):
+            def __call__(self, s): ...
+        sentence = 'I am a dog.'
+        with self.assertRaises(NotImplementedError):
+            Dummy(Tokenizer(), WordEmbedding()).embed(sentence)
+
+    def test_call_not_implemented(self):
+        class Dummy(SentenceEmbedding):
+            def embed(self, s): ...
+        sentence = 'I am a dog.'
+        with self.assertRaises(NotImplementedError):
+            Dummy(Tokenizer(), WordEmbedding()).__call__(sentence)
 
 
 class MeanEmbeddingCase(TestCase):
 
     def setUp(self):
         self.sentence_embedding = MeanEmbedding()
-        pass
 
     def tearDown(self):
         pass
 
     def test_embed(self):
         sentence = "I am a dog."
-        vector = self.sentence_embedding.embed(sentence)
+        vector = self.sentence_embedding(sentence)
         self.assertEqual(vector.shape, (300,))
