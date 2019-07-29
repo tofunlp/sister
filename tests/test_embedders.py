a4b321139cabb406d42be2dcc9ccc8de2b63bf20
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-07-29 13:44:50 +0900

    Naiive solution for word embedding test (for 100% test coverage).

--- tests/test_embedders.py
+++ tests/test_embedders.py
@@ -1,9 +1,6 @@
 from unittest import TestCase
-from unittest.mock import patch
 
-import numpy as np
-
-from sister.word_embedders import WordEmbedding
+from sister.word_embedders import WordEmbedding, FasttextEmbedding
 
 
 class WordEmbeddingCase(TestCase):
@@ -32,23 +29,30 @@ class WordEmbeddingCase(TestCase):
 class FasttextEmbeddingCase(TestCase):
 
     def setUp(self):
-        embedding_patcher = patch('sister.word_embedders.FasttextEmbedding')
-        self.embedding = embedding_patcher.start()(lang='en')
-        self.embedding.get_word_vector.return_value = np.random.rand(300)
-        self.embedding.get_word_vectors.side_effect = lambda words: np.random.rand(len(words), 300)
-        self.embedding_patcher = embedding_patcher
+        # TODO
+        # Downloading fasttext and unzipping is too heavy.
+        # Needs to be mocked somehow.
+        # For now, solved with caching.
+
+        # embedding_patcher = patch('sister.word_embedders.FasttextEmbedding')
+        # self.embedding = embedding_patcher.start()(lang='en')
+        # self.embedding.get_word_vector.return_value = np.random.rand(300)
+        # self.embedding.get_word_vectors.side_effect = lambda words: np.random.rand(len(words), 300)
+        # self.embedding_patcher = embedding_patcher
+        self.embedding = FasttextEmbedding(lang="en")
 
     def tearDown(self):
-        self.embedding_patcher.stop()
+        # self.embedding_patcher.stop()
+        pass
 
     def test_get_word_vector(self):
         word = 'test'
         vector = self.embedding.get_word_vector(word)
         self.assertTupleEqual(vector.shape, (300,))
-        self.embedding.get_word_vector.assert_called_once_with(word)
+        # self.embedding.get_word_vector.assert_called_once_with(word)
 
     def test_get_word_vectors(self):
         words = ['good', 'test']
         vectors = self.embedding.get_word_vectors(words)
         self.assertTupleEqual(vectors.shape, (len(words), 300))
-        self.embedding.get_word_vectors.assert_called_once_with(words)
+        # self.embedding.get_word_vectors.assert_called_once_with(words)
