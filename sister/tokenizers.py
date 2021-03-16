16ac75e9f1b71d433c85b5b6709b63b6f7765357
sobamchan <oh.sore.sore.soutarou@gmail.com>
2021-03-16 15:38:34 +0900

    format by black and isort

--- sister/tokenizers.py
+++ sister/tokenizers.py
@@ -1,30 +1,30 @@
 from typing import List
+
 from janome.tokenizer import Tokenizer as JanomeTokenizer
 
 
 class Tokenizer(object):
-
     def tokenize(self, sentence: str) -> List[str]:
         raise NotImplementedError
 
 
 class SimpleTokenizer(Tokenizer):
-
     def __init__(self):
-        self.replace_tokens = str.maketrans({
-            '.': ' .',
-            '?': ' ?',
-            '!': ' !',
-            '(': ' ( ',
-            ')': ' ) ',
-        })
+        self.replace_tokens = str.maketrans(
+            {
+                ".": " .",
+                "?": " ?",
+                "!": " !",
+                "(": " ( ",
+                ")": " ) ",
+            }
+        )
 
     def tokenize(self, sentence: str) -> List[str]:
         return sentence.translate(self.replace_tokens).split()
 
 
 class JapaneseTokenizer(Tokenizer):
-
     def __init__(self):
         self._t = JanomeTokenizer()
 
