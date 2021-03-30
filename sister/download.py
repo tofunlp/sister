90d4cda8e2de8dd9d2c5e47e3aa43f648016cd42
sobamchan <oh.sore.sore.soutarou@gmail.com>
2021-03-30 11:18:07 +0900

    format by black and isort

--- sister/download.py
+++ sister/download.py
@@ -1,28 +1,28 @@
-import tempfile
+import bz2
 import contextlib
+import gzip
 import hashlib
 import os
 import shutil
 import sys
+import tempfile
 import zipfile
-import bz2
-import gzip
-import progressbar
-from urllib import request
 from pathlib import Path
+from urllib import request
 
+import progressbar
 
 pbar = None
 
 
 _cache_root = os.environ.get(
-    'SISTER_CACHE_ROOT',
-    os.path.join(os.path.expanduser('~'), '.sister', 'cache'))
+    "SISTER_CACHE_ROOT", os.path.join(os.path.expanduser("~"), ".sister", "cache")
+)
 
 
 @contextlib.contextmanager
 def tempdir(**kwargs):
-    ignore_errors = kwargs.pop('ignore_errors', False)
+    ignore_errors = kwargs.pop("ignore_errors", False)
 
     temp_dir = tempfile.mkdtemp(**kwargs)
     try:
@@ -40,8 +40,7 @@ def set_cache_root(path: str) -> None:
     _cache_root = path
 
 
-def get_cache_directory(cache_name: str,
-                        create_directory: bool = True) -> str:
+def get_cache_directory(cache_name: str, create_directory: bool = True) -> str:
     path = os.path.join(_cache_root, cache_name)
     if create_directory:
         try:
@@ -67,22 +66,22 @@ def show_progress(block_num, block_size, total_size):
 
 
 def cached_download(url: str) -> str:
-    cache_root = os.path.join(_cache_root, '_dl_cache')
+    cache_root = os.path.join(_cache_root, "_dl_cache")
     try:
         os.makedirs(cache_root)
     except OSError:
         if not os.path.isdir(cache_root):
             raise
 
-    urlhash = hashlib.md5(url.encode('utf-8')).hexdigest()
+    urlhash = hashlib.md5(url.encode("utf-8")).hexdigest()
     cache_path = os.path.join(cache_root, urlhash)
 
     if os.path.exists(cache_path):
         return cache_path
 
     with tempdir(dir=cache_root) as temp_root:
-        temp_path = os.path.join(temp_root, 'dl')
-        sys.stderr.write('Downloading from {}...\n'.format(url))
+        temp_path = os.path.join(temp_root, "dl")
+        sys.stderr.write("Downloading from {}...\n".format(url))
         sys.stderr.flush()
         request.urlretrieve(url, temp_path, show_progress)
         shutil.move(temp_path, cache_path)
@@ -92,21 +91,21 @@ def cached_download(url: str) -> str:
 
 def cached_unzip(path: Path, saveto: Path) -> None:
     if not saveto.exists():
-        with zipfile.ZipFile(path, 'r') as f:
+        with zipfile.ZipFile(path, "r") as f:
             f.extractall(saveto)
 
 
 def cached_decompress_bz2(path: Path, saveto: Path) -> None:
     if not saveto.exists():
-        with bz2.open(path, 'rb') as fin:
+        with bz2.open(path, "rb") as fin:
             _content = fin.read()
-        with open(saveto, 'wb') as fout:
+        with open(saveto, "wb") as fout:
             fout.write(_content)
 
 
 def cached_decompress_gzip(path: Path, saveto: Path) -> None:
     if not saveto.exists():
-        with gzip.open(path, 'rb') as fin:
+        with gzip.open(path, "rb") as fin:
             _content = fin.read()
-        with open(saveto, 'wb') as fout:
+        with open(saveto, "wb") as fout:
             fout.write(_content)
